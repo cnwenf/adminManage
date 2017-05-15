7450508f34f59acbdc1317eda07c77bc6678a319
SSEWenFeng <wenfengsse@outlook.com>
2017-05-15 17:37:38 +0800

    项目列表修复

--- src/main/webapp/pages/js/lib/unicorn.dashboard.js
+++ src/main/webapp/pages/js/lib/unicorn.dashboard.js
@@ -15,42 +15,42 @@ $(document).ready(function(){
         sin.push([i, Math.sin(i)]);
         cos.push([i, Math.cos(i)]);
     }
-
-	// === Make chart === //
-    var plot = $.plot($(".chart"),
-           [ { data: sin, label: "sin(x)", color: "#BA1E20"}, { data: cos, label: "cos(x)",color: "#459D1C" } ], {
-               series: {
-                   lines: { show: true },
-                   points: { show: true }
-               },
-               grid: { hoverable: true, clickable: true },
-               yaxis: { min: -1.6, max: 1.6 }
-		   });
-    
-	// === Point hover in chart === //
-    var previousPoint = null;
-    $(".chart").bind("plothover", function (event, pos, item) {
-		
-        if (item) {
-            if (previousPoint != item.dataIndex) {
-                previousPoint = item.dataIndex;
-                
-                $('#tooltip').fadeOut(200,function(){
-					$(this).remove();
-				});
-                var x = item.datapoint[0].toFixed(2),
-					y = item.datapoint[1].toFixed(2);
-                    
-                unicorn.flot_tooltip(item.pageX, item.pageY,item.series.label + " of " + x + " = " + y);
-            }
-            
-        } else {
-			$('#tooltip').fadeOut(200,function(){
-					$(this).remove();
-				});
-            previousPoint = null;           
-        }   
-    });	
+    //
+    // // === Make chart === //
+    // var plot = $.plot($(".chart"),
+    //        [ { data: sin, label: "sin(x)", color: "#BA1E20"}, { data: cos, label: "cos(x)",color: "#459D1C" } ], {
+    //            series: {
+    //                lines: { show: true },
+    //                points: { show: true }
+    //            },
+    //            grid: { hoverable: true, clickable: true },
+    //            yaxis: { min: -1.6, max: 1.6 }
+		//    });
+    //
+    // // === Point hover in chart === //
+    // var previousPoint = null;
+    // $(".chart").bind("plothover", function (event, pos, item) {
+		//
+    //     if (item) {
+    //         if (previousPoint != item.dataIndex) {
+    //             previousPoint = item.dataIndex;
+    //
+    //             $('#tooltip').fadeOut(200,function(){
+		// 			$(this).remove();
+		// 		});
+    //             var x = item.datapoint[0].toFixed(2),
+		// 			y = item.datapoint[1].toFixed(2);
+    //
+    //             unicorn.flot_tooltip(item.pageX, item.pageY,item.series.label + " of " + x + " = " + y);
+    //         }
+    //
+    //     } else {
+		// 	$('#tooltip').fadeOut(200,function(){
+		// 			$(this).remove();
+		// 		});
+    //         previousPoint = null;
+    //     }
+    // });
     
     // === Calendar === //    
     var date = new Date();
--- src/main/webapp/pages/js/project-overview.js
+++ src/main/webapp/pages/js/project-overview.js
@@ -2,7 +2,7 @@
  * Created by WenFe on 2017/4/18.
  */
 var app = angular.module('projectOverviewApp', []);
-app.controller('projectOverviewCtrl', function($scope) {
+app.controller('projectOverviewCtrl', function($scope, $http) {
     $scope.userInfo = getLocalJson("userInfo");
     $(document).ready(function () {
 
@@ -28,12 +28,22 @@ app.controller('projectOverviewCtrl', function($scope) {
     };
 
     function getProjectList() {
+        $http({
+            method: 'POST',
+            url: api.project.list,
+            data: $.param({companyId: $scope.userInfo.companyId}),
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' }
+        }).success(function(data) {
+            if (data) {
+                $scope.projects = data;
+            }
+            });
         var callBackFuc = function(data) {
             if (data) {
                 $scope.projects = data;
             }
         };
-        httpSyncPost(api.project.list, {companyId: $scope.userInfo.companyId}, callBackFuc);
+        //httpSyncPost(api.project.list, {companyId: $scope.userInfo.companyId}, callBackFuc);
     }
 
     function init() {
